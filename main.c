ac2578ec558753b5b6f960731c6434bc3e8400f9
杨光 <yangguang@yangguangdeMacBook-Pro.local>
2019-03-04 11:14:37 +0800

    optimized

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <memory.h>
+#include <stdlib.h>
 
 int count = 0;
-const int SIZE = 6;
-int column_index[SIZE];
+const int SIZE = 8;
+int * column_index;
+
 
 int check(int row, int col) {
 
@@ -33,17 +34,77 @@ void print() {
     }
 }
 
-void init() {
+int * init(unsigned size) {
+
+    column_index = malloc(sizeof(int) * size);
 
     for (int i = 0; i < SIZE; ++i) {
 
         column_index[i] = -1;
     }
+
+    return column_index;
 }
 
-void eight_queens() {
+void destroy(){
 
-    init();
+    if(column_index != NULL){
+
+        free(column_index);
+
+        column_index = NULL;
+    }
+}
+
+void eight_queens_ok() {
+
+    init(SIZE);
+
+    int r = 0, c = 0;
+
+    for (; r < SIZE;) {
+
+        for (; c < SIZE; ++c) {
+
+            if (check(r, c) == 1) {
+
+                column_index[r] = c;
+                c = 0;
+                break;
+            }
+        }
+
+        if(c == SIZE && r == 0){
+
+            break;
+        } else if (column_index[r] == -1) {
+
+            --r;
+            c = column_index[r] + 1;
+            column_index[r] = -1;
+
+        } else {
+
+            ++r;
+            if (r == SIZE) {
+
+                printf("%d\n", ++count);
+                print();
+
+                --r;
+                c = column_index[r] + 1;
+                column_index[r] = -1;
+            }
+        }
+    }
+
+    destroy();
+
+}
+
+void eight_queens_test() {
+
+    init(SIZE);
 
     int r, c;
 
@@ -82,17 +143,19 @@ void eight_queens() {
                     printf("%d\n", ++count);
                     print();
 
-                    -- r;
+                    --r;
                     c = column_index[r] + 1;
                     column_index[r] = -1;
                 }
             }
         }
     }
+
+    destroy();
 }
 
 int main() {
 
-    eight_queens();
+    eight_queens_ok();
     return 0;
 }
